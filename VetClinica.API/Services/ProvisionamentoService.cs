540ab6590a9bfe31dc2fd5c66fcb8a0d5787bb27
rotelloguerra15 <rotelloguerra15@gmail.com>
2026-06-17 16:09:24 -0300

    fix: remove replicacao tenant no schema

--- VetClinica.API/Services/ProvisionamentoService.cs
+++ VetClinica.API/Services/ProvisionamentoService.cs
@@ -1,4 +1,4 @@
-PS C:\Projetos\vetclinica> Get-Content C:\Projetos\vetclinica\VetClinica.API\Services\ProvisionamentoService.cs | Select-Object -Index (59..79)
+﻿PS C:\Projetos\vetclinica> Get-Content C:\Projetos\vetclinica\VetClinica.API\Services\ProvisionamentoService.cs | Select-Object -Index (59..79)
             SchemaName = schema,
             CriadoEm  = DateTime.UtcNow,
             Ativo      = true
@@ -6,13 +6,11 @@ PS C:\Projetos\vetclinica> Get-Content C:\Projetos\vetclinica\VetClinica.API\Ser
         _platform.Tenants.Add(tenant);
         await _platform.SaveChangesAsync();
 
-        // 4. Seeds dentro do schema recÃ©m-criado
+        // 4. Seeds dentro do schema recÃƒÂ©m-criado
         using var db = _factory.CreateForSchema(schema);
 
         // Replica o registro do tenant no schema do tenant
         // (para queries cross-schema em controllers como GestaoVistaController)
-        db.Tenants.Add(tenant);
-        await db.SaveChangesAsync();
 
         var senhaTemp = GerarSenhaTemporaria();
         var owner = new User
